ec7822812029182dd986ea1b6247e224b74b8b8a
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2022-08-21 12:50:40 -0300

    working with John 3:16

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { useState } from "react";
+import api from "./api";
 
 function App() {
-    const api = "https://bible-api.com/";
     const [book, setBook] = useState("");
     const [chapter, setChapter] = useState("");
     const [verse, setVerse] = useState("");
+    const [result, setResult] = useState("");
 
     return (
         <div className="App">
@@ -13,9 +14,19 @@ function App() {
             <input type="text" onChange={(e) => setChapter(e.target.value)} />
             <input type="text" onChange={(e) => setVerse(e.target.value)} />
 
-            <button onClick={() => {
-              console.log(api + "%20")
-            }}>Submit</button>
+            <button
+                onClick={() => {
+                    api.get("/john%203:16?translation=almeida")
+                        .then((response) => setResult(response.data))
+                        .catch((err) => {
+                            console.error("ops! ocorreu um erro" + err);
+                        });
+
+                    console.log(result.verses[0].text);
+                }}
+            >
+                Submit
+            </button>
         </div>
     );
 }
